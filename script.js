bd7a572e05191bff0936cc6ac960d9fb0f79e20b
Glitch (p5-js-template-cssi-2020) <none>
2020-07-14 04:05:52 +0000

    🎌👣 Checkpoint ./index.html:15945984/9 ./script.js:15945984/1680

--- script.js
+++ script.js
@@ -5,15 +5,63 @@
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
           keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 
-let backgroundColor;
+let dot1;
 
 function setup() {
-  // Canvas & color settings
-  createCanvas(400, 400);
+  createCanvas(windowWidth - 20, windowHeight - 20);
   colorMode(HSB, 360, 100, 100);
-  backgroundColor = 95;
+  dot1 = new BouncyDot();
 }
 
 function draw() {
-  background(backgroundColor);
+  background(220, 0, 80);
+  dot1.float();
+  dot1.display();
+}
+
+function mousePressed() {
+  // We'll use this for console log statements only.
+  console.log(dot1.x);
+}
+
+class BouncyDot {
+  constructor() {
+    // Randomly generate position
+    this.x = random(width);
+    this.y = random(height);
+    // Randomly generate radius
+    this.r = random(5, 12);
+    // Randomly generate color
+    this.color = random(360);
+    // Randomly generate a master velocity (broken into components)...
+    this.masterXvelocity = random(0.5, 3);
+    this.masterYvelocity = random(0.5, 3);
+    // ...and use those as starting velocities.
+    this.xVelocity = this.masterXvelocity;
+    this.yVelocity = this.masterYvelocity;
+  }
+
+  float() {
+    this.x += this.xVelocity;
+    this.y += this.yVelocity;
+    // Standard bounce code - like the DVD logo, but for spheres.
+    if (this.x + this.r > width) {
+      this.xVelocity = -1 * this.masterXvelocity;
+    }
+    if (this.x - this.r < 0) {
+      this.xVelocity = this.masterXvelocity;
+    }
+    if (this.y + this.r > height) {
+      this.yVelocity = -1 * this.masterYvelocity;
+    }
+    if (this.y - this.r < 0) {
+      this.yVelocity = this.masterYvelocity;
+    }
+  }
+
+  display() {
+    fill(this.color, 80, 70);
+    noStroke();
+    ellipse(this.x, this.y, this.r * 2);
+  }
 }
